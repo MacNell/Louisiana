fc9abbee7df85a7e8e7d76c29f634e3c69e32818
Nathaniel MacNell <macnell.nathaniel@gmail.com>
2015-03-12 13:10:10 -0400

    Restriction cleanup
    Fix restriction mechanism

--- script/lib/restrict.R
+++ script/lib/restrict.R
@@ -1,40 +1,27 @@
-# restrict.R
+### restrict.R
 # functions to restrict the set of points to worry about
 
-# function to find the "diameter" of a geometry
-# really, the diagonal of its bounding box
-# this is more efficient and minially increases search area
 gDiameter <- function(poly){
+  # Find a simple diameter bound of a geometry
+  # (the diagonal of its bounding box)
     sqrt(sum((poly@bbox[,2]-poly@bbox[,1])^2))
 }
 
-# function to restrict points to points of interest
-# if no buffer distance, return points relevant for closest-distance
-# this is 2.5 times the diameter distance from the centroid
-RestrictPoints <- function(poly,points,d=0) {
 
-	# first ,establish true search distance
-    if(d==0) {  
-		# if no distance is specified, use 2.5 times diameter
-        d <- 2.5*gDiameter(poly)
-    }
-    else{
-		# increase search distance by diameter of polygon
-		# helps handle elongated polygons
-        d <- d+gDiameter(poly)
-    }
-
-	# calculate distance from centroid to points
-    dists <- spDistsN1(points,gCentroid(poly))
-    close <- dists<d
-
-    if(sum(close)==0) {  
-		#if there are no points, return closest point(s)
-        points <- points[dists==min(dists),]
-    }
-    else{
-		# return points of interest.
-        points <- points[close,]
-    }
-    return(points)
+RestrictPoints <- function(polygon,points,d=0) {
+  # Restrict potential points to those in a region of interest.
+  # Points relevant to buffer: distance plus polygon diameter
+  d <- d + gDiameter(polygon)
+  
+  # find flags for points within buffer distance
+  distances <- spDistsN1(points,gCentroid(polygon))
+  inbuffer  <- distances < d
+  
+  # Expand search to points that could influence minimum distance
+  # these are within one diameter of identified minimum.
+  d2 <- min(distances) + gDiameter(polygon)
+  proximate <- distances < d2
+  
+  return(points[inbuffer | proximate,])
+  
 }
